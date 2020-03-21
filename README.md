1dbd7b48bed0f69c164be53133dbbd28d6ef5f17
Holger Karl <hkarl@ieee.org>
2020-03-21 19:36:48 +0100

    changed to more up-to-date data provider. TODO: fix CLI parameters

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # coronaStats
 Visualize corona statistics 
+
+Download case numbers and visualize; logarithmic plots over countries 
+
--- corona.py
+++ corona.py
@@ -1,16 +1,16 @@
 """Download and visualiaze corona numbers.
 Based on: 
-- Corona data from https://covid.ourworldindata.org/data/full_data.csv
-- Possibly better source: https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases/resource/4cd2eaa1-fd3e-4371-a234-a8ef2b44cc1f 
+- Use https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series 
 - Population data from https://datahub.io/JohnSnowLabs/population-figures-by-country (more up to date useful)
 """
 
-import csv, sys
+import csv, sys, io
 
 from pprint import pprint as pp
 from collections import defaultdict 
 from itertools import groupby, dropwhile
 from math import log10 
+import datetime
 
 import click 
 import requests
@@ -19,121 +19,206 @@ import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
+baseUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/" 
+filenames = (
+    "time_series_19-covid-Confirmed.csv",
+    "time_series_19-covid-Deaths.csv",
+    "time_series_19-covid-Recovered.csv", 
+    )
 
+
+thresholds = {
+    "Confirmed": {'Days': 15, 'Cases': 50}, 
+    "Confirmed_relative": {'Days': 15, 'Cases': 50},
+    # 
+    "Deaths": {'Days': 5, 'Cases': 10}, 
+    "Deaths_relative": {'Days': 5, 'Cases': 10},
+    # 
+    "Recovered": {'Days': 10, 'Cases': 20}, 
+    "Recovered_relative": {'Days': 10, 'Cases': 20}, 
+    }
+    
 class NotImplemented(Exception):
     pass
 
 
+   
+def load_population(countries_file):
+    with open(countries_file, "r", newline='') as f:
+        next(f)  # skip header 
+        reader = csv.reader(f)
+        countries = [r for r in reader]
 
-def get_data(download, countries_file, corona_file, url):
-    """Download and write to file, or just load from file. 
-    Return two lists of values: population numbers and corona statistics"""
-    
-    def download_files(url, corona_file):
-        r = requests.get(url)
-        with open(corona_file, 'wb') as f:
-            f.write(r.content)
+    return countries 
 
-    def load_files():
-        with open(countries_file, "r", newline='') as f:
-            next(f)  # skip header 
-            reader = csv.reader(f)
-            countries = [r for r in reader]
+def download_files(baseUrl, filenames):
+    data = {}
+    for fn in filenames: 
+        r = requests.get(baseUrl + fn)
+        data[fn] = str(r.text)
+        with open(fn, 'w') as f:
+            f.write(data[fn])
 
-        with open(corona_file, "r", newline='') as f:
-            next(f)
-            reader = csv.DictReader(f,
-                fieldnames=["date", "country", "new_cases", "new_deaths", "total_cases", "total_deaths"])
-            corona = [r for r in reader]
+    return data 
+        
+def load_files(filenames):
+    data = {}
+    for fn in filenames: 
+        with open(fn, "r") as f:
+            data[fn] = f.read()
+
+    return data 
+
+def get_csv(data):
+    """ turn raw data into useful CSV output"""
+
+    myint = lambda s: int(s) if s.isdigit() else s
+
+    dataout = {}
+    for k, v in data.items():
+        reader = csv.DictReader(io.StringIO(v), delimiter=",")
+        temp_entries = [r for r in reader]
+
+        temp_entries_with_dates = []
+        
+        # Some immediate cleanup:
+        for r in temp_entries:
+            # ensure consistency with population data: 
+            if r['Country/Region'] == 'Korea, South':
+                r['Country/Region'] = 'South Korea'
+
+            # turn numbers into numbers; dates into dates 
+            tmp = {'Country/Region': r['Country/Region'],
+                    'Province/State': r['Province/State'],
+                    'dates': defaultdict(int),
+                    }
+                
+            for kk, vv in r.items():
+                #  dates into dates, get the integer value:
+                try:
+                    kk_date = datetime.datetime.strptime(kk, "%m/%d/%y")
+                    tmp['dates'][kk_date] = int(vv)
+                except Exception:
+                    pass
+
+            temp_entries_with_dates.append(tmp)
+
+        # Summarize for China, US:
+        summarize_countries = ("China", "US")
+        for s in summarize_countries:
+            counties = [r for r in temp_entries_with_dates if r['Country/Region'] == s]
+            new_entry = counties[0].copy()
+            new_entry['Province/State'] = ""
+            new_entry['dates'] = {}
+            relevant_dates = set([x for r in counties for x in list(r['dates'].keys()) ])
+            # pp(relevant_dates)
             
-        return (countries, corona)
+            for kk in relevant_dates:
+                new_entry['dates'][kk] = sum( r['dates'][kk] for r in counties  )
+            temp_entries_with_dates.append(new_entry)
+
+        # filter out provinces
+        actual_key = k[1+k.rfind('-'):-4]
+        dataout[actual_key] = [r for r in temp_entries_with_dates if r['Province/State'] == ""]
+        
+        
+    pp(dataout)
+    # pp(dataout.keys())
+    return dataout
 
 
-    if download:
-        raw_csv = download_files(url, corona_file)
-
-    raw_csv = load_files()
-
-    return raw_csv
-
-
-def process_csv(raw_csv):
+def process_csv(population_data, corona_csv):
     """Clean up numbers, compute ratios proportional to population size"""
 
-    myint = lambda s: int(s) if s.isdigit() else 0
+    print("=======================")
 
-    raw_countries, raw_corona = raw_csv
-
-    countries = dict( ( (x[0], x[-1]) for x in raw_countries) ) 
+    countries = dict( ( (x[0], x[-1]) for x in population_data) ) 
 
     corona = defaultdict(dict)
 
-    for country, countrygroup in groupby(raw_corona, lambda x: x['country']):
-        if country == "International":
-            # That is just those cruise ships, not so interesting 
-            continue 
-        
-        it = dropwhile(lambda x: x['new_cases'] == '' or int(x['new_cases']) < 50, countrygroup)
-        first = next(it, None)
-        if first: 
-            corona[country] = {'start': first['date'],
-                                'new_cases': [myint(first['new_cases'])],
-                                'new_deaths': [myint(first['new_deaths'])],
-                                'total_cases': [myint(first['total_cases'])],
-                                'total_deaths': [myint(first['total_deaths'])],
-                                'population': int(countries[first['country']])
-                                              if first['country'] in countries else 0,
-                              }
+    for statistics_key, countrylist in corona_csv.items():
+        for country in countrylist:
+            countryname = country['Country/Region']
+            print(statistics_key, country['Country/Region']) 
+            # only consider country if it has at least a certain mimimum number of days 
+            if len(country['dates'].keys()) < thresholds[statistics_key]['Days']:
+                continue
 
-            for rest in it:
-                corona[country]['new_cases'].append(myint(rest['new_cases']))
-                corona[country]['new_deaths'].append(myint(rest['new_deaths']))
-                corona[country]['total_cases'].append(myint(rest['total_cases']))
-                corona[country]['total_deaths'].append(myint(rest['total_deaths']))
+            # only consider country if it has a certain minimum number of cases
+            if max(country['dates'].values()) < thresholds[statistics_key]['Cases']:
+                continue
 
-            if corona[country]['population'] > 0: 
-                for k in ('new_cases', 'new_deaths', 'total_cases', 'total_deaths'):
-                    try: 
-                        corona[country][k + "_relative"] = [float(x)/corona[country]['population']
-                                                        if x > 0 else 0
-                                                        for x in corona[country][k] ]
-                        corona[country][k + "_relative_log"] = [log10(float(x))/corona[country]['population']
-                                                        if x > 0 else 0
-                                                        for x in corona[country][k] ]
-                    except ValueError:
-                        print(corona[country][k])
+            values = [(k, country['dates'][k])
+                          for k in sorted(country['dates'].keys())
+                          if country['dates'][k] > thresholds[statistics_key]['Cases']
+                          ]
 
+            if len(values) < thresholds[statistics_key]['Days']:
+                continue
+            
+            try:
+                population = int(countries[countryname])
+            except Exception:
+                population = 0
 
-    return corona 
+            corona[countryname].update({
+                'start': values[0][0],
+                'population': population})
+            corona[countryname][statistics_key] = [v[1] for v in values] 
+            corona[countryname][statistics_key + "_relative"] =  [float(v[1])/float(population) for v in values ] if population > 0 else None 
+            
+
+    pp(corona)
+    return corona
 
 
 def visualize(corona, mincases, mindays):
 
-    fields = (        
-        ('total_cases', 'Total cases', 'Total', 0, 0,),
-        ('total_deaths', 'Total deaths',  'Total', 0, 1, ),
-        ('total_cases_relative', 'Total cases, relative to population', "Relative to population", 1, 0, ),
-        ('total_deaths_relative', 'Total deaths, relative to population', "Relative to population", 1, 1, ),
+    fields = (
+        # key, titel, label, subplot positions 
+        ('Confirmed', 'Total cases', 'Total', 0, 0,),
+        ('Deaths', 'Total deaths',  'Total', 0, 1, ),
+        ('Confirmed_relative', 'Total cases, relative to population', "Relative to population", 1, 0, ),
+        ('Deaths_relative', 'Total deaths, relative to population', "Relative to population", 1, 1, ),
+        ('Recovered', 'Recovered cases', "Total", 0, 2, ),
+        ('Recovered_relative', 'Recovered, relative to population', "Relative to population", 1, 2, ),
         )
 
-    fig, ax = plt.subplots(2, 2)
+    fig, ax = plt.subplots(2, 3)
 
     for field, title, valuelabel, x, y,  in fields: 
 
-        ax[x][y].set_title(title)
-        ax[x][y].set_xlabel("Days since {} total cases".format(mincases))
+        ax[x][y].set_title(title + " (at least {} days)".format(
+            thresholds[field]['Days']) )        
+        ax[x][y].set_xlabel("Days since {} cases".format(thresholds[field]['Cases']))
         ax[x][y].set_ylabel(valuelabel)
 
-        ax[x][y].set_prop_cycle(color=plt.cm.Spectral([x/12 for x in range(12)]),
-                          marker=['o', '+', 'x'] * 4)
+        
+        count = len([1
+                        for country, values in corona.items()
+                        if field in values
+                        and not values[field] == None
+                        and len(values[field]) >= mindays
+                    ])
+                    
 
+        color_count = count // 3 + 1 
+        ax[x][y].set_prop_cycle(color=plt.cm.Spectral([float(x)/(color_count*3) for x in range(color_count*3)]),
+                          marker=['o', '+', 'x'] * color_count )
+
+        print (count, color_count) 
+        
         for country, values in corona.items():
 
             if field not in values:
                 print("Skipping, value not present : ", country, field)
                 continue
-            if len(values[field]) < mindays:
-                print("Skipping, country not enough days : ", country)
+            if values[field] == None:
+                print("Skipping, value is None: ", country, field)
+                continue
+            
+            if len(values[field]) < thresholds[field]['Days']:
+                print("Skipping, country not enough days : ", country, field)
                 continue
             
             ax[x][y].semilogy(values[field], label=country)
@@ -153,8 +238,19 @@ def visualize(corona, mincases, mindays):
 @click.option('-d', '--days', default=10, help="Minimum number of days (after threshold) for which data must be available bbefore considering country in visualization")
 # @click.option('-s', '--status', default=False, help="Show status information")
 def main(download, population, corona, mincases, url, days):
-    raw_csv = get_data(download, population, corona, url)
-    data = process_csv(raw_csv)
+    if download:
+        raw_data = download_files(baseUrl, filenames)
+    else:
+        raw_data = load_files(filenames) 
+
+    population_data = load_population(population)
+
+    corona_csv = get_csv(raw_data)
+
+    data = process_csv(population_data, corona_csv)
+
+    pp(data)
+    
     visualize(data, mincases, days) 
 
 if __name__ == '__main__':
